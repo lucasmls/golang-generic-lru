99b0715448ee2331fc9483fc43296291a3998e9b
Lucas Mendes <lucaslsbh1@gmail.com>
2022-03-28 07:45:51 -0300

    Add LRU logic into Cache

--- lru.go
+++ lru.go
@@ -1,6 +1,7 @@
 package lru
 
 import (
+	"container/list"
 	"errors"
 )
 
@@ -9,12 +10,14 @@ var (
 )
 
 type entry[T any] struct {
+	Key   string
 	Value T
 }
 
 type LRU[T any] struct {
-	capacity int
-	storage  map[string]*entry[T]
+	capacity     int
+	storage      map[string]*list.Element
+	evictionList list.List
 }
 
 func New[T any](capacity int) (*LRU[T], error) {
@@ -22,26 +25,60 @@ func New[T any](capacity int) (*LRU[T], error) {
 		return nil, InvalidStorageSize
 	}
 
+	var v list.List
 	c := &LRU[T]{
-		capacity: capacity,
-		storage:  make(map[string]*entry[T], capacity),
+		capacity:     capacity,
+		storage:      make(map[string]*list.Element, capacity),
+		evictionList: v,
 	}
 
 	return c, nil
 }
 
-func (c *LRU[T]) Add(key string, value T) {
-	c.storage[key] = &entry[T]{
-		Value: value,
+func (c *LRU[T]) Add(key string, value T) (evicted bool) {
+	if el, ok := c.storage[key]; ok {
+		en := el.Value.(*entry[T])
+		en.Value = value
+
+		c.evictionList.MoveToFront(el)
+
+		return false
 	}
+
+	evict := c.evictionList.Len() >= c.capacity
+	if evict {
+		el := c.evictionList.Back()
+		if el != nil {
+			c.evictionList.Remove(el)
+		}
+
+		en, ok := el.Value.(*entry[T])
+		if !ok {
+			return false
+		}
+
+		delete(c.storage, en.Key)
+	}
+
+	en := &entry[T]{key, value}
+	el := c.evictionList.PushFront(en)
+	c.storage[key] = el
+
+	return evict
 }
 
-func (c *LRU[T]) Get(key string) (T, bool) {
-	entry, ok := c.storage[key]
+func (c *LRU[T]) Get(key string) (t T, ok bool) {
+	el, ok := c.storage[key]
 	if !ok {
-		var t T
 		return t, false
 	}
 
-	return entry.Value, true
+	en, ok := el.Value.(*entry[T])
+	if !ok {
+		return t, false
+	}
+
+	c.evictionList.MoveToFront(el)
+
+	return en.Value, true
 }
--- lru_test.go
+++ lru_test.go
@@ -1,12 +1,14 @@
 package lru
 
 import (
+	"container/list"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_New(t *testing.T) {
+	var l list.List
 	t.Run("Failure tests", func(t *testing.T) {
 		tt := []struct {
 			name     string
@@ -37,8 +39,9 @@ func Test_New(t *testing.T) {
 			{
 				name: "Should be able to instantiate a LRU with the correct storage capacity",
 				want: &LRU[string]{
-					capacity: 10,
-					storage:  make(map[string]*entry[string], 10),
+					capacity:     10,
+					storage:      make(map[string]*list.Element, 10),
+					evictionList: l,
 				},
 				capacity: 10,
 			},
@@ -57,20 +60,34 @@ func Test_New(t *testing.T) {
 
 func Test_Add(t *testing.T) {
 	tt := []struct {
-		name  string
-		key   string
-		value string
+		name       string
+		key        string
+		value      string
+		beforeEach func(cache *LRU[string])
 	}{
 		{
 			name:  "Should be able to include a new entry into cache",
 			key:   "users-1",
 			value: "John",
 		},
+		{
+			name:  "Should be able to update a given entry",
+			key:   "users-1",
+			value: "John Wick",
+			beforeEach: func(cache *LRU[string]) {
+				cache.Add("users-1", "John")
+			},
+		},
 	}
+
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			cache, err := New[string](3)
 			assert.NoError(t, err)
+
+			if tc.beforeEach != nil {
+				tc.beforeEach(cache)
+			}
 
 			cache.Add(tc.key, tc.value)
 			entry, ok := cache.Get(tc.key)
@@ -138,4 +155,34 @@ func Test_Get(t *testing.T) {
 			})
 		}
 	})
+}
+
+func Test_LRU(t *testing.T) {
+	cache, err := New[string](3)
+	assert.NoError(t, err)
+
+	evicted := cache.Add("pets/dog", "Dog")
+	assert.False(t, evicted)
+
+	evicted = cache.Add("pets/cat", "Cat")
+	assert.False(t, evicted)
+
+	evicted = cache.Add("pets/bird", "Bird")
+	assert.False(t, evicted)
+
+	evicted = cache.Add("pets/salamander", "Salamander")
+	assert.True(t, evicted)
+
+	_, ok := cache.Get("pets/dog")
+	assert.False(t, ok)
+
+	value, ok := cache.Get("pets/cat")
+	assert.True(t, ok)
+	assert.Equal(t, value, "Cat")
+
+	evicted = cache.Add("pets/shark", "Shark")
+	assert.True(t, evicted)
+
+	value, ok = cache.Get("pets/bird")
+	assert.False(t, ok)
 }
